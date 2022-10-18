03b6cc374682db2a33da9b3c24a3ad909132792e
Cintia Ruiz <cintiaruizv@gmail.com>
2022-10-18 13:41:05 -0500

    connecting to movebike DB

---
+++ index.js
@@ -0,0 +1,15 @@
+import dbConnect from './src/libs/db.js'
+import {server} from './src/server.js'
+
+
+dbConnect()
+    .then(() => {
+        console.log('MOVEBIKE database coneccted');
+
+        server.listen(8080, () => {
+            console.log('Server listening on port 8080');
+        })
+
+
+    })
+    .catch((error) => console.log('Error: ', error))
